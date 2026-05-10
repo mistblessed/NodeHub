1d8a063a73a6117ddd325ac2f81cd0bb538d8017
Pashok <dadaf@gmail.com>
2026-05-10 13:00:28 +0300

    add About and Contacts

--- app/__init__.py
+++ app/__init__.py
@@ -45,5 +45,13 @@ def create_app():
     @app.route('/')
     def index():
         return render_template('index.html')
+    
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
+
+    @app.route('/contacts')
+    def contacts():
+        return render_template('contacts.html')
 
     return app
